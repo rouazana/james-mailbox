bd45095ba27d224297503133c567c24f76323078
Eric Charles <eric@apache.org>
2013-04-26 13:09:19 +0000

    Add svn:ignore
    git-svn-id: https://svn.apache.org/repos/asf/james/mailbox/trunk@1476179 13f79535-47bb-0310-9956-ffa450edef68

--- caching/src/main/java/org/apache/mailbox/caching/CacheLoaderFromUnderlying.java
+++
@@ -1,5 +0,0 @@
-package org.apache.mailbox.caching;
-
-public interface CacheLoaderFromUnderlying<Key, Value, Underlying, Except extends Throwable> {
-	Value load(Key key, Underlying underlying) throws Except;
-}
